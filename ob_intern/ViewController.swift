cc2ed4a7d6fd2ba4aab692d8a94f91f8aaf10cb4
Soft Liampisan <soft.liampisan@gmail.com>
2023-06-07 10:42:55 +0700

    Create new project for ob_intern

---
+++ ob_intern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ob_intern
+//
+//  Created by Soft Liampisan on 7/6/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
