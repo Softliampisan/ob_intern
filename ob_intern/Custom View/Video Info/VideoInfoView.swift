3ac5148d82a7312e4531e9ab1140b58d98ad14ce
Soft Liampisan <soft.liampisan@gmail.com>
2023-06-23 09:05:05 +0700

    Update: custom text view class

--- ob_intern/Custom View/Video Info/VideoInfoView.swift
+++ ob_intern/Custom View/Video Info/VideoInfoView.swift
@@ -15,7 +15,7 @@ class VideoInfoView: InitializeXibView {
     @IBOutlet weak var imageViewProfilePic: UIImageView!
     @IBOutlet weak var labelProfileName: UIView!
     @IBOutlet weak var labelPostTime: UILabel!
-    @IBOutlet weak var textViewInfo: UITextView!
+    @IBOutlet weak var textViewInfo: CustomTextView!
     @IBOutlet weak var viewHashtag: UIView!
     
     private var storedText: String = ""
@@ -33,13 +33,16 @@ class VideoInfoView: InitializeXibView {
     private func setupView() {
         setupContainerView()
         setupStackView()
-        textViewInfo.delegate = self
-        textViewInfo.isSelectable = true
-        textViewInfo.isEditable = false
-        textViewInfo.layoutIfNeeded()
-        textViewInfo.translatesAutoresizingMaskIntoConstraints = true
-        textViewInfo.sizeToFit()
-        setPublicPrivateAttribute()
+//        textViewInfo.delegate = self
+//        textViewInfo.isSelectable = true
+//        textViewInfo.isEditable = false
+//        textViewInfo.layoutIfNeeded()
+//        textViewInfo.translatesAutoresizingMaskIntoConstraints = true
+//        textViewInfo.sizeToFit()
+//        setPublicPrivateAttribute()
+        //textViewInfo = CustomTextView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        let text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proid"
+        textViewInfo.setAttributedTextWithTruncation(text)
     }
 
 
@@ -109,23 +112,23 @@ class VideoInfoView: InitializeXibView {
 
 extension VideoInfoView: UITextViewDelegate {
     
-    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
-        if URL.scheme == "more" {
-            print("more is pressed")
-            textViewInfo.text = storedText
-
-            return false
-        }
-        else {
-            return true
-        }
-    }
+//    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+//        if URL.scheme == "more" {
+//            print("more is pressed")
+//            textViewInfo.text = storedText
+//
+//            return false
+//        }
+//        else {
+//            return true
+//        }
+//    }
 }
 extension String {
     
-    func attributed(using font: UIFont) -> NSAttributedString {
-        return NSAttributedString(string: self, attributes: [.font: font])
-    }
+//    func attributed(using font: UIFont) -> NSAttributedString {
+//        return NSAttributedString(string: self, attributes: [.font: font])
+//    }
     func truncate(to length: Int, trailing: String = "...") -> (truncated: String, remaining: String) {
                guard self.count > length else {
                    return (self, "")
