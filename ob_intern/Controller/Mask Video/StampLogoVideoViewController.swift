6ed0c495588d221797aae930449a25aa1d5bed4c
Pakkapon Kidpaisansup <pakkapon@ookbee.com>
2023-07-14 10:21:05 +0700

    Update: code

--- ob_intern/Controller/Mask Video/StampLogoVideoViewController.swift
+++ ob_intern/Controller/Mask Video/StampLogoVideoViewController.swift
@@ -41,7 +41,7 @@ class StampLogoVideoViewController: UIViewController {
     var composition = AVMutableVideoComposition()
     private var isAssetLoaded = false
     let playerViewController = AVPlayerViewController()
-
+    
     
     
     //MARK: - Lifecycle
@@ -101,47 +101,241 @@ class StampLogoVideoViewController: UIViewController {
         }
     }
     
-    func addComposition(asset: AVAsset, image: UIImage, url: URL){
+    func test(currentAsset: AVAsset, videoCompositionSoft: AVMutableVideoComposition) {
+        let composition = AVMutableComposition.init()
+        
+        let videoComposition = AVMutableVideoComposition()
+        videoComposition.frameDuration = CMTimeMake(value: 1, timescale: 30)
+        videoComposition.renderScale  = 1.0
+        
+        let compositionCommentaryTrack: AVMutableCompositionTrack? = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        
+        let compositionVideoTrack: AVMutableCompositionTrack? = composition.addMutableTrack(withMediaType: AVMediaType.video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        
+        let clipVideoTrack:AVAssetTrack = currentAsset.tracks(withMediaType: AVMediaType.video)[0]
+        
+        let audioTrack: AVAssetTrack? = currentAsset.tracks(withMediaType: AVMediaType.audio)[0]
+        
+        try? compositionCommentaryTrack?.insertTimeRange(CMTimeRangeMake(start: CMTime.zero, duration: currentAsset.duration), of: audioTrack!, at: CMTime.zero)
+        
+        try? compositionVideoTrack?.insertTimeRange(CMTimeRangeMake(start: CMTime.zero, duration: currentAsset.duration), of: clipVideoTrack, at: CMTime.zero)
+//
+//        let orientation = VideoModel.videoOrientation(currentAsset)
+//        var isPortrait = false
+//
+//        switch orientation {
+//        case .landscapeRight:
+//            isPortrait = false
+//        case .landscapeLeft:
+//            isPortrait = false
+//        case .portrait:
+//            isPortrait = true
+//        case .portraitUpsideDown:
+//            isPortrait = true
+//        }
+//
+        var naturalSize = clipVideoTrack.naturalSize
+        
+//        if isPortrait
+//        {
+//            naturalSize = CGSize.init(width: naturalSize.height, height: naturalSize.width)
+//        }
+        
+        videoCompositionSoft.renderSize = naturalSize
+        
+        let scale = CGFloat(1.0)
+        
+        var transform = CGAffineTransform.init(scaleX: CGFloat(scale), y: CGFloat(scale))
+        
+//        switch orientation {
+//        case .landscapeRight: break
+//            // isPortrait = false
+//        case .landscapeLeft:
+//            transform = transform.translatedBy(x: naturalSize.width, y: naturalSize.height)
+//            transform = transform.rotated(by: .pi)
+//        case .portrait:
+//            transform = transform.translatedBy(x: naturalSize.width, y: 0)
+//            transform = transform.rotated(by: CGFloat(M_PI_2))
+//        case .portraitUpsideDown:break
+//        }
+        
+        let frontLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: compositionVideoTrack!)
+        frontLayerInstruction.setTransform(transform, at: CMTime.zero)
+        
+        let MainInstruction = AVMutableVideoCompositionInstruction()
+        MainInstruction.timeRange = CMTimeRangeMake(start: CMTime.zero, duration: composition.duration)
+        MainInstruction.layerInstructions = [frontLayerInstruction]
+        videoComposition.instructions = [MainInstruction]
+        
+        let parentLayer = CALayer.init()
+        parentLayer.frame = CGRect.init(x: 0, y: 0, width: naturalSize.width, height: naturalSize.height)
+        
+        let videoLayer = CALayer.init()
+        videoLayer.frame = parentLayer.frame
+        
+        
+        let layer = CATextLayer()
+        layer.string = "HELLO ALL"
+        layer.foregroundColor = UIColor.white.cgColor
+        layer.backgroundColor = UIColor.orange.cgColor
+        layer.fontSize = 32
+        layer.frame = CGRect.init(x: 0, y: 0, width: 300, height: 100)
+        
+        var rct = layer.frame;
+        
+        let widthScale = self.viewVideo.frame.size.width/naturalSize.width
+        
+        rct.size.width /= widthScale
+        rct.size.height /= widthScale
+        rct.origin.x /= widthScale
+        rct.origin.y /= widthScale
+        
+        
+        
+        parentLayer.addSublayer(videoLayer)
+        parentLayer.addSublayer(layer)
+        
+        videoComposition.animationTool = AVVideoCompositionCoreAnimationTool.init(postProcessingAsVideoLayer: videoLayer, in: parentLayer)
+        
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let videoPath = documentsPath+"/cropEditVideo.mov"
+        
+        let fileManager = FileManager.default
+        
+        if fileManager.fileExists(atPath: videoPath)
+        {
+            try! fileManager.removeItem(atPath: videoPath)
+        }
+        
+        print("video path \(videoPath)")
+        
+        var exportSession = AVAssetExportSession.init(asset: composition, presetName: AVAssetExportPresetHighestQuality)
+        exportSession?.outputFileType = AVFileType.mov
+        exportSession?.outputURL = URL.init(fileURLWithPath: videoPath)
+        exportSession?.videoComposition = videoCompositionSoft
+        var exportProgress: Float = 0
+        let queue = DispatchQueue(label: "Export Progress Queue")
+        queue.async(execute: {() -> Void in
+            while exportSession != nil {
+                //                int prevProgress = exportProgress;
+                exportProgress = (exportSession?.progress)!
+                print("current progress == \(exportProgress)")
+                sleep(1)
+            }
+        })
+        
+        exportSession?.exportAsynchronously(completionHandler: {
+            
+            
+            if exportSession?.status == AVAssetExportSession.Status.failed
+            {
+                print("Failed \(exportSession?.error)")
+            }else if exportSession?.status == AVAssetExportSession.Status.completed
+            {
+                exportSession = nil
+                
+                let asset = AVAsset.init(url: URL.init(fileURLWithPath: videoPath))
+                DispatchQueue.main.async {
+                    let item = AVPlayerItem.init(asset: asset)
+                    
+                    let textItem = AVPlayerItem(asset: asset)
+//                                    textItem.videoComposition = export.videoComposition
+                                    DispatchQueue.main.async { // Execute on main queue
+                                        self.createVideoController(playerItem: textItem)
+                                    }
+                    
+//                    self.player?.replaceCurrentItem(with: item)
+//
+//                    let assetDuration = CMTimeGetSeconds(composition.duration)
+//                    self.progressSlider.maximumValue = Float(assetDuration)
+                    
+//                    self.syncLayer.removeFromSuperlayer()
+//                    self.lblIntro.isHidden = true
+                    
+//                    self.player.play()
+                    //                    let url =  URL.init(fileURLWithPath: videoPath)
+                    //                    let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: [])
+                    //                    self.present(activityVC, animated: true, completion: nil)
+                }
+                
+            }
+        })
        
+    }
+    
+    //    func ConvertAvcompositionToAvasset(avComp: AVComposition, completion:@escaping (_ avasset: AVAsset) -> Void){
+    //        let exporter = AVAssetExportSession(asset: avComp, presetName: AVAssetExportPresetHighestQuality)
+    //        let randNum:Int = Int(arc4random())
+    //        //Generating Export Path
+    //        let exportPath: NSString = NSTemporaryDirectory().appendingFormat("\(randNum)"+"video.mov") as NSString
+    //        let exportUrl: NSURL = NSURL.fileURL(withPath: exportPath as String) as NSURL
+    //        //SettingUp Export Path as URL
+    //        exporter?.outputURL = exportUrl as URL
+    //        exporter?.outputFileType = .mov
+    //        exporter?.shouldOptimizeForNetworkUse = true
+    //        exporter?.exportAsynchronously(completionHandler: {() -> Void in
+    //            DispatchQueue.main.async(execute: {() -> Void in
+    //                if exporter?.status == .completed {
+    //                    let URL: URL? = exporter?.outputURL
+    //                    let Avasset:AVAsset = AVAsset(url: URL!)
+    //                    completion(Avasset)
+    //                }
+    //                else if exporter?.status == .failed {
+    //                    print("Failed")
+    //
+    //                }
+    //            })
+    //        }) }
+    
+    func addComposition(asset: AVAsset, image: UIImage, url: URL){
+        
         composition = AVMutableVideoComposition(asset: asset) { request in
             print("request is entered")
-
-            //UIImage(systemName: "globe.europe.africa.fill")
-            let textImage = image
-            let imageExample = CIImage(image: textImage)
-
-            let positionedText = imageExample?.transformed(by: CGAffineTransform(translationX: request.renderSize.width/2, y: 200))
+            
+            //            let textImage = UIImage(systemName: "globe.europe.africa.fill")!
+            //            let textImage = image
+            let imageExample = CIImage(image: image)
+            
+            //            let positionedText = imageExample?.transformed(by: CGAffineTransform(translationX: 0, y: 0))
+            let positionedText = imageExample?.transformed(by: .identity, highQualityDownsample: true)
             request.finish(with: (positionedText?.composited(over: request.sourceImage))!, context: nil)
         }
-     
-        let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetMediumQuality)
-        exporter?.outputFileType = .mov
-        exporter?.outputURL = url
-        print("outputURL \(exporter?.outputURL)")
-        exporter?.videoComposition = composition
-        exporter?.exportAsynchronously { [weak exporter] in
-            guard let export = exporter else {
-                return
-            }
-            
-            switch export.status {
-            case  .failed:
-                print("failed \(export.error)")
-                break
-            case .cancelled:
-                print("cancelled \(exporter?.error)")
-                break
-            case .completed:
-                print("complete")
-                let textItem = AVPlayerItem(asset: export.asset)
-                textItem.videoComposition = self.composition
-                DispatchQueue.main.async { // Execute on main queue
-                    self.createVideoController(playerItem: textItem)
-                }
-            default:
-                print("default")
-            }
-        }
+        
+        test(currentAsset: asset, videoCompositionSoft: composition)
+
+        
+        //        let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetMediumQuality)
+        //        exporter?.outputFileType = .mov
+        //        exporter?.outputURL = url
+        //        print("outputURL \(exporter?.outputURL)")
+        ////        exporter?.videoComposition = composition
+        ////        exporter?.customVideoCompositor = composition
+        //
+        //        exporter?.exportAsynchronously(completionHandler: { [weak self] in
+        //            guard let export = exporter else {
+        //                return
+        //            }
+        //
+        //            switch export.status {
+        //            case  .failed:
+        //                print("failed \(export.error)")
+        //                break
+        //            case .cancelled:
+        //                print("cancelled \(exporter?.error)")
+        //                break
+        //            case .completed:
+        //                print("complete")
+        //                let textItem = AVPlayerItem(asset: export.asset)
+        //                textItem.videoComposition = export.videoComposition
+        //                DispatchQueue.main.async { // Execute on main queue
+        //                    self?.createVideoController(playerItem: textItem)
+        //                }
+        //            default:
+        //                print("default")
+        //            }
+        //        })
     }
     
     
@@ -149,7 +343,7 @@ class StampLogoVideoViewController: UIViewController {
         
         let asset = AVAsset(url: url)
         addComposition(asset: asset, image: image, url: url)
-
+        
         
     }
     
@@ -165,11 +359,11 @@ class StampLogoVideoViewController: UIViewController {
         if let customText = addTextView?.exportTextImage() {
             print("exported image \(customText)")
             createCustomText(url: videoURL! as URL, image: customText)
-
+            
         }
         do { // delete old video
             try FileManager.default.removeItem(at: videoURL! as URL)
-            } catch { print(error.localizedDescription) }
+        } catch { print(error.localizedDescription) }
         
     }
     
